78c2ce84de62e4956c5dd3ca38ab19834717741a
preciousadeniyi <135836301+preciousadeniyi@users.noreply.github.com>
2026-06-19 21:46:07 +0100

    Update __init__.py

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,1 +1,13 @@
 
+
+
+
+"""Generative models and variational inference."""
+
+from pvf_u.models.generative_model import GenerativeModel
+from pvf_u.models.variational import VariationalModel
+
+__all__ = [
+    "GenerativeModel",
+    "VariationalModel",
+]
