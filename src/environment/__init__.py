bee0eaf4cc6c33b2d3149dfc78fbd46b54d728cf
preciousadeniyi <135836301+preciousadeniyi@users.noreply.github.com>
2026-06-19 21:51:10 +0100

    Update __init__.py

--- src/environment/__init__.py
+++ src/environment/__init__.py
@@ -1,1 +1,11 @@
+"""Environments for PVF-U evaluation."""
 
+from pvf_u.environment.shape_game import ShapeGame
+from pvf_u.environment.cooperative_navigation import CooperativeNavigation
+from pvf_u.environment.dark_room import DarkRoom
+
+__all__ = [
+    "ShapeGame",
+    "CooperativeNavigation",
+    "DarkRoom",
+]
