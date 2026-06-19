308d2c1fc482d2390c85d9aca2070a8f06f5fce9
preciousadeniyi <135836301+preciousadeniyi@users.noreply.github.com>
2026-06-19 21:23:40 +0100

    Update __init__.py

--- src/core/__init__.py
+++ src/core/__init__.py
@@ -1,1 +1,12 @@
 
+"""Core PVF-U implementation."""
+
+from pvf_u.core.agent import PVFAgent
+from pvf_u.core.valence import compute_total_valence
+from pvf_u.core.narrative import NarrativeEmbedding
+
+__all__ = [
+    "PVFAgent",
+    "compute_total_valence",
+    "NarrativeEmbedding",
+]
